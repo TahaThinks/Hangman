9742e35aff6a54122eff3862461064737049baed
TahaThinks <tahaa4587@gmail.com>
2024-01-08 09:30:54 +0400

    check if the letter is in chose_word

--- hangman.py
+++ hangman.py
@@ -4,8 +4,15 @@ import random
 word_list = ["aardvark", "baboon", "camel"]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-word = random.choice(word_list)
+chosen_word = random.choice(word_list)
+print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-letter = input("Enter a letter: ").lower()
-print(letter)
+guess = input("Enter a letter: ").lower()
+
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+for i in range(len(chosen_word)):
+    if (guess == chosen_word[i]):
+        print("YES")
+        break
+    
+
