ac17b903928343314370f68d82f1066166cab73a
TahaThinks <tahaa4587@gmail.com>
2024-01-08 10:09:48 +0400

    Display the Guess if correct

--- hangman.py
+++ hangman.py
@@ -11,8 +11,10 @@ print(f'Pssst, the solution is {chosen_word}.')
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 display = []
-for dashes in range(len(chosen_word)):
-    display.append('_')
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+    display += '_'
 print(display)
 
 guess = input("Guess a letter: ").lower()
@@ -20,13 +22,12 @@ guess = input("Guess a letter: ").lower()
 #TODO-2: - Loop through each position in the chosen_word;
 #If the letter at that position matches 'guess' then reveal that letter in the display at that position.
 #e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for position in range(word_length):
+    if chosen_word[position] == guess:
+        #print("Right")
+        display[position] = guess
 
 #TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
 #Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-    
+print(display)
 
