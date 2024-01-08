7f37b44d33671f2d3b744b9f56420cbf3d68b593
TahaThinks <tahaa4587@gmail.com>
2024-01-08 09:36:51 +0400

    Refine code

--- hangman.py
+++ hangman.py
@@ -7,12 +7,13 @@ word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 print(chosen_word)
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Enter a letter: ").lower()
+guess = input("Guess a letter: ").lower()
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-for i in range(len(chosen_word)):
-    if (guess == chosen_word[i]):
-        print("YES")
-        break
+for letter in chosen_word:
+    if (letter == guess):
+        print("Right")
+    else:
+        print("Wrong")
     
 
